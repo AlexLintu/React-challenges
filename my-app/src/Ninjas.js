7de6b616ea6a775933d7a4587db2de04d45ac972
Alexandra Tkachenko <osatkachenko@gmail.com>
2018-09-18 18:55:35 -0700

    First app

---
+++ my-app/src/Ninjas.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Ninjas extends Component {
+    render() {
+        return(
+            <div className="ninja">
+                <div>Name: Alex</div>
+                <div>Age: 123</div>
+                <div>Belt: Black</div>
+            </div>
+        )
+    }
+}
+
+export default Ninjas;
