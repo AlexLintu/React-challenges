bca50e8c73fb000412ab53d150bcbc2e5ce5bf86
Alexandra Tkachenko <osatkachenko@gmail.com>
2018-09-20 15:28:29 -0700

    Added array of values

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react';
 import Ninjas from './Ninjas';
 
 class App extends Component {
+  state = {
+    ninjas: [
+      {name: 'A', age: 30, belt: 'balck', id: 1},
+      {name: 'B', age: 40, belt: 'green', id: 2},
+      {name: 'C', age: 50, belt: 'pink', id: 3}
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>My first React app!</h1>
         <p>Welcome! :)</p>
-        <Ninjas name="Alex" age="123" belt="Black"/>
+        <Ninjas ninjas={this.state.ninjas} />
       </div>
     );
   }
