79babe5859882a1c320df2fbee4bccf0de73a549
Alexandra Tkachenko <osatkachenko@gmail.com>
2018-09-21 10:47:44 -0700

    Added form file

---
+++ my-app/src/AddNinja.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class AddNinja extends Component {
+    render() {
+        return (
+            <div>
+                <form></form>
+            </div>
+        )
+    }
+}
