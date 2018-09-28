2ce0d5cbf423d89de552d87b0b85f8290bdb7a09
Alexandra Tkachenko <osatkachenko@gmail.com>
2018-09-28 14:57:01 -0700

    Change Inline CSS Conditionally Based on Component State

---
+++ changing-inline-CSS-conditionally-based-on-component-state.jsx
@@ -0,0 +1,38 @@
+
+class GateKeeper extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    this.setState({ input: event.target.value })
+  }
+  render() {
+    let inputStyle = {
+      border: '1px solid black'
+    };
+    /* Solution #1
+    if (this.state.input.length > 15) {
+      inputStyle = {border: '3px solid red'}
+    } else {
+      inputStyle;
+    } */
+    
+    // Solution #2
+    this.state.input.length > 15 ? inputStyle = {border: '3px solid red'} : inputStyle
+
+    return (
+      <div>
+        <h3>Don't Type Too Much:</h3>
+        <input
+          type="text"
+          style={inputStyle}
+          value={this.state.input}
+          onChange={this.handleChange} />
+      </div>
+    );
+  }
+};
