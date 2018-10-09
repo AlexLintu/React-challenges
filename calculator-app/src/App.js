52f27a66e53b8edb82875d37f5eaafe4af0b0d15
Alexandra Tkachenko <osatkachenko@gmail.com>
2018-10-09 11:28:39 -0700

    Removed files

--- calculator-app/src/App.js
+++ calculator-app/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
+import React, { Component } from 'react';
+import './index.css';
+
 class App extends Component {
   render() {
     return (
       <div>
-
+Hello
       </div>
     );
   }
