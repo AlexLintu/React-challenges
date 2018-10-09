903da9bd5a486753602bde98c62fe5e8f503984a
Alexandra Tkachenko <osatkachenko@gmail.com>
2018-10-09 14:17:51 -0700

    Edited App file

--- calculator-app/src/App.js
+++ calculator-app/src/App.js
@@ -4,11 +4,14 @@ import React, { Component } from 'react';
 import './index.css';
 
 class App extends Component {
+  state = {
+    displayValue: '0'
+  }
   render() {
+    const { displayValue } = this.state
+
     return (
-      <div>
-Hello
-      </div>
+
     );
   }
 }
