7b5924a1fa76fb7833c2e35f87790fed69dbd483
Alexandra Tkachenko <osatkachenko@gmail.com>
2018-10-10 10:53:07 -0700

    Adjusted Apps file

--- calculator-app/src/App.js
+++ calculator-app/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-
-import React, { Component } from 'react';
 import './index.css';
+
+// 1. What state is there?
+// 2. When does it change?
 
 class App extends Component {
   state = {
@@ -11,7 +12,10 @@ class App extends Component {
     const { displayValue } = this.state
 
     return (
+      <Display />
+      <div className="digit-keys">
 
+      </div>
     );
   }
 }
