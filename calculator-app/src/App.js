30aeb43c2263a5fec19d778a519c0deb7a068deb
Alexandra Tkachenko <osatkachenko@gmail.com>
2018-10-11 12:37:23 -0700

    Adjusted CSS to add Grid instead of Display

--- calculator-app/src/App.js
+++ calculator-app/src/App.js
@@ -47,35 +47,25 @@ class App extends Component {
     return (
       <div className="calculator">
         <div className="calculator-display">{displayValue}</div>
-        <div className="calculator-keypad">
-          <div className="input-keys">
-            <div className="function-keys">
-                <button className="calculator-key key-clear" onClick={() => this.clearDisplay()}>AC</button>
-                <button className="calculator-key key-sign" onClick={() => this.toggleSign()}>±</button>
-                <button className="calculator-key key-percent">%</button>
-            </div>
-            <div className="digit-keys">
-                <button className="calculator-key key-0" onClick={() => this.inputDigit(0)}>0</button>
-                <button className="calculator-key key-dot" onClick={() => this.inputDot()}>.</button>
-                <button className="calculator-key key-1" onClick={() => this.inputDigit(1)}>1</button>
-                <button className="calculator-key key-2" onClick={() => this.inputDigit(2)}>2</button>
-                <button className="calculator-key key-3" onClick={() => this.inputDigit(3)}>3</button>
-                <button className="calculator-key key-4" onClick={() => this.inputDigit(4)}>4</button>
-                <button className="calculator-key key-5" onClick={() => this.inputDigit(5)}>5</button>
-                <button className="calculator-key key-6" onClick={() => this.inputDigit(6)}>6</button>
-                <button className="calculator-key key-7" onClick={() => this.inputDigit(7)}>7</button>
-                <button className="calculator-key key-8" onClick={() => this.inputDigit(8)}>8</button>
-                <button className="calculator-key key-9" onClick={() => this.inputDigit(9)}>9</button>
-            </div>
-            <div className="operator-keys">
-                <button className="calculator-key key-divide">+</button>
-                <button className="calculator-key key-multiply">x</button>
-                <button className="calculator-key key-subtract">-</button>
-                <button className="calculator-key key-add">+</button>
-                <button className="calculator-key key-equal">=</button>
-            </div>
-          </div>
-        </div>
+        <button className="calculator-key key-clear" onClick={() => this.clearDisplay()}>AC</button>
+        <button className="calculator-key key-sign" onClick={() => this.toggleSign()}>±</button>
+        <button className="calculator-key key-percent">%</button>
+        <button className="calculator-key key-divide">+</button>
+        <button className="calculator-key key-7" onClick={() => this.inputDigit(7)}>7</button>
+        <button className="calculator-key key-8" onClick={() => this.inputDigit(8)}>8</button>
+        <button className="calculator-key key-9" onClick={() => this.inputDigit(9)}>9</button>
+        <button className="calculator-key key-multiply">x</button>
+        <button className="calculator-key key-4" onClick={() => this.inputDigit(4)}>4</button>
+        <button className="calculator-key key-5" onClick={() => this.inputDigit(5)}>5</button>
+        <button className="calculator-key key-6" onClick={() => this.inputDigit(6)}>6</button>
+        <button className="calculator-key key-subtract">-</button>
+        <button className="calculator-key key-1" onClick={() => this.inputDigit(1)}>1</button>
+        <button className="calculator-key key-2" onClick={() => this.inputDigit(2)}>2</button>
+        <button className="calculator-key key-3" onClick={() => this.inputDigit(3)}>3</button>
+        <button className="calculator-key key-add">+</button>
+        <button className="calculator-key key-0" onClick={() => this.inputDigit(0)}>0</button>
+        <button className="calculator-key key-dot" onClick={() => this.inputDot()}>.</button>
+        <button className="calculator-key key-equal">=</button>             
       </div>
     );
   }
