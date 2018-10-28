43ad6258977e29c0111b83e6b27cc05f1dc40424
Alexandra Tkachenko <osatkachenko@gmail.com>
2018-10-27 17:51:20 -0700

    Adding conditionals

---
+++ codeCademy/using-a-conditional-in-a-render-function.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const fiftyFifty = Math.random() < 0.5;
+
+class TonightsPlan extends React.Component {
+  render() {
+    let plan;
+    if (fiftyFifty) {
+      plan = "Tonight I'm going out WOOO"
+    } else {
+      plan = "Tonight I'm going to bed WOOO"
+    }
+    
+    return <h1>{plan}</h1>;
+  }
+}
+
+ReactDOM.render(
+	<TonightsPlan />,
+	document.getElementById('app')
+);
+
+// Second solytion:
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const fiftyFifty = Math.random() < 0.5;
+
+class TonightsPlan extends React.Component {
+  render() {
+    if (fiftyFifty) {
+      return <h1>Tonight I'm going out WOOO</h1>
+    } else {
+      return <h1>Tonight I'm going to bed WOOO</h1>
+    }
+  }
+}
+
+ReactDOM.render(
+	<TonightsPlan />,
+	document.getElementById('app')
+);
