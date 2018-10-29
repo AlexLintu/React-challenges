90f17c079f95ad6f1d118428d96285d936d61703
Alexandra Tkachenko <osatkachenko@gmail.com>
2018-10-29 10:42:35 -0700

    Accessing props

---
+++ codeCademy/8-accessing-a-components-props.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class PropsDisplayer extends React.Component {
+  render() {
+    // Component's `props` is an object that holds information about that component:
+  	const stringProps = JSON.stringify(this.props); // `JSON.stringify()` method: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify 
+
+    return (
+      <div>
+        <h1>CHECK OUT MY PROPS OBJECT</h1>
+        <h2>{stringProps}</h2>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+	<PropsDisplayer />,
+	document.getElementById('app')
+);
