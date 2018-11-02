2ac524b15318a8bcd6c0a1e2a0235865c079a0a4
Alexandra Tkachenko <osatkachenko@gmail.com>
2018-11-02 13:22:25 -0700

    Working with styles in React

--- codeCademy/21-making-a-style-object-variable.js
+++ codeCademy/21-making-a-style-object-variable.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 
 // Adding all of the style properties to a variable:
 const styles = {
-  background: 'lightblue', 
-  color: 'darkred'
-}
+    background: 'lightblue', 
+    color: 'darkred',
+    // Using camelCase for style names in React - `marginTop` instead of `margin-top`:
+    marginTop: '100px',
+    // Also, values that are using `px` can be written without quotation marks - fontSize: 50, if the unit is other than `px` quotation marks are required - "2em":
+    fontSize: 50
+  }
 // Using that variable in JSX:
 const styleMe = <h1 style={styles}>Please style me! I am so bland!</h1>;
 
