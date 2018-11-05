b32eb9a48d25b11de34f5fa34c1f5f6477447f00
Alexandra Tkachenko <osatkachenko@gmail.com>
2018-11-04 20:06:52 -0800

    Controlled components

--- codeCademy/28-updating-an-inputs-value.js
+++ codeCademy/28-updating-an-inputs-value.js
@@ -20,7 +20,7 @@ export class Input extends React.Component {
   render() {
     return (
       <div>
-        {/*onChange is event listener that waits for a change and calls event handler `this.handleUserInput`, which updates state
+        {/*onChange is event listener that waits for a change and calls event handler `handleUserInput`, which updates state.
         `value` makes sure that <input /> and `userInput` are linked together and responsive/ same on each change */}
         <input type="text" onChange={this.handleUserInput} value={this.state.userInput}/>
         <h1>{this.state.userInput}</h1> 
---
+++ codeCademy/29-controlled-vs-uncontrolled.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export class Input extends React.Component {
+  render() {
+    return (
+      <div>
+        <input type="text" />
+        <h1>I am an h1.</h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+	<Input />,
+	document.getElementById('app')
+);
+
+// Forms: https://reactjs.org/docs/forms.html 
